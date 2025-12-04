f86241dc95bd61bd1d02a4f28f0f92ebf61d7dd5
Missiler <albinrik@gmail.com>
2025-12-04 12:25:05 +0100

    new try

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'console_scripts': [
             'uwb_read_only_node = uwb_positioning.reader:main',
             'uwb_read_only_node_launch = uwb_positioning.runner:main',
-            'uwb_odom_broadcaster = uwb_positioning.uwb_publisher:main',
+            'uwb_publisher = uwb_positioning.uwb_publisher:main',
         ],
     },
 )
